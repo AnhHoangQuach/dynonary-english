68c4487efefd771aa7b7760cbbd73b2d73c8924b
AnhHoangQuach <hoanganh36.work@gmail.com>
2023-08-14 22:30:49 +0700

    feat: backend

--- frontend/.prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  bracketSpacing: true,
-  singleQuote: true,
-  jsxBracketSameLine: true,
-  trailingComma: "all",
-  printWidth: 80,
-  parser: "babel",
-  arrowParens: "always",
-};
