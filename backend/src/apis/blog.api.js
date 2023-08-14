4b7c21ba573c5aa463b57599d231cb0225020a6c
AnhHoangQuach <hoanganh36.work@gmail.com>
2023-08-14 23:10:52 +0700

    feat: frontend

--- backend/src/apis/blog.api.js
+++
@@ -1,8 +0,0 @@
-const blogApi = require('express').Router();
-const blogController = require('../controllers/blog.controller');
-
-blogApi.get('/blog-list', blogController.getBlogList);
-
-blogApi.get('/blog-html', blogController.getBlogHtml);
-
-module.exports = blogApi;
--- backend/src/apis/flashcard.api.js
+++
@@ -1,6 +0,0 @@
-const flashcardApi = require('express').Router();
-const flashcardController = require('../controllers/flashcard.controller');
-
-flashcardApi.get('/word-pack', flashcardController.getWordPack);
-
-module.exports = flashcardApi;
